c0221002b5fd7a68825eff05f75b9ee1940a8582
Pedro <pedrompecanha27@gmail.com>
2023-01-14 20:08:01 -0300

    test

---
+++ MovieAgenda/Model/Network/Secrets.swift
@@ -0,0 +1,11 @@
+//
+//  Secrets.swift
+//  MovieAgenda
+//
+//  Created by Ricardo Venieris on 17/10/22.
+//
+
+import Foundation
+
+let tmdbKey = "95b336b6369a2ad868d199034d34fc0c"
+let tmdbToken = "Adicione seu token"
